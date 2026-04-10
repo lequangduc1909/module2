f5f72ea25eb52af4da8121f2d9aaaae9e4d634f6
lequangduc1909 <157683044+lequangduc1909@users.noreply.github.com>
2026-04-10 08:20:01 +0700

    Add files via upload

---
+++ xay_dung_lop_chi_ghi/Main.java
@@ -0,0 +1,9 @@
+package xay_dung_lop_chi_ghi;
+public class Main {
+    public static void main(String[] args) {
+        Student hocsinh1 = new Student();
+
+        hocsinh1.setName("Lê Quang Đức");
+        hocsinh1.setClasses("C19");
+    }
+}
