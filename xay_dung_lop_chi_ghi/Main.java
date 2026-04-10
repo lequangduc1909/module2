5a816d75a38ebe05cc033a5d78cb03a018b1aed3
lequangduc1909 <157683044+lequangduc1909@users.noreply.github.com>
2026-04-10 08:22:44 +0700

    Delete xay_dung_lop_chi_ghi directory

--- xay_dung_lop_chi_ghi/Main.java
+++
@@ -1,9 +0,0 @@
-package xay_dung_lop_chi_ghi;
-public class Main {
-    public static void main(String[] args) {
-        Student hocsinh1 = new Student();
-
-        hocsinh1.setName("Lê Quang Đức");
-        hocsinh1.setClasses("C19");
-    }
-}
